d21426b84bb767d0f89fb324e491fd2044901e15
Vivek Bunker <vivekbunker65@gmail.com>
2026-02-24 11:40:07 +0530

    cors

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ const permissionRoutes = require("./src/routes/permission.routes");
 const contactreply = require("./src/routes/reply.routes");
 // Middleware
 app.use(cors({
-    // origin: ["*"],
-    origin: [
-        "http://localhost:3000",
-        "http://localhost:5173",
-        "https://staffmanagement-superadmin.vercel.app"
-    ],
+    origin: ["*"],
+    // origin: [
+    //     "http://localhost:3000",
+    //     "http://localhost:5173",
+    //     "https://staffmanagement-superadmin.vercel.app"
+    // ],
     credentials: true
 }));
 app.use(express.json());
